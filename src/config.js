60af36035c3ce3496be7d3088557431399c9048d
hudziec <ahudziec@gmail.com>
2019-05-21 14:44:27 -0400

    “”

---
+++ src/config.js
@@ -0,0 +1,2 @@
+const API_KEY = '6b7f8444afa9e66229065f174da5c3dc';
+export default API_KEY
---
+++ src/views/racer/index.js
@@ -0,0 +1,12 @@
+import React, { Component }from 'react';
+import './index.css';
+
+class Racer extends Component {
+  render() {
+    return (
+      <h1>Racer</h1>
+    );
+  }
+}
+
+export default Racer;
